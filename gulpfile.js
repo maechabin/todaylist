137119a85d68e36e6e19625c80713362da394072
unknown <t_maeda@RN0176.fancs.com>
2015-04-01 11:53:11 +0900

    cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('uglify', function () {
 	gulp.src('src/js/todaylist.js')
 		.pipe(uglify())
-		.pipe(gulp.dest('build'))
+		.pipe(rename('todaylist.min.js'))
+		.pipe(gulp.dest('dist'))
 });
 
 gulp.task('default', ['uglify']);
